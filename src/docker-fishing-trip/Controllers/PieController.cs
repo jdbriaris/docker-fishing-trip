88feff5e6ceada319545e8e32e2bed167b839eeb
jdbriaris <james.briaris@gmail.com>
2017-02-02 16:00:09 +0000

    Added basic view showing a list of pies

---
+++ src/docker-fishing-trip/Controllers/PieController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using docker_fishing_trip.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace docker_fishing_trip.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository; 
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(_pieRepository.Pies);
+        }
+    }
+}
--- src/docker-fishing-trip/Startup.cs
+++ src/docker-fishing-trip/Startup.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using docker_fishing_trip.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -16,22 +12,19 @@ namespace docker_fishing_trip
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ICategoryRepository, MockCategoryRepository>();
+            services.AddTransient<IPieRepository, MockPieRepository>();
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDeveloperExceptionPage(); // Adds support for showing exceptions in the browser
+            app.UseStatusCodePages(); // Adds the ability to handle response codes bewteen 400 and 600
+            app.UseStaticFiles(); // Adds the ability to serve static files
+            app.UseMvcWithDefaultRoute(); // Use the default routing mechanism
         }
     }
 }
